c6de715b6affdc01f0055852ae29c95b6632b707
renandfe22 <lealrenan@hotmail.com>
2023-11-14 18:35:50 -0300

    Projeto concluído

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-/*Se vitórias for menor do que 10 = Ferro
-Se vitórias for entre 11 e 20 = Bronze
-Se vitórias for entre 21 e 50 = Prata
-Se vitórias for entre 51 e 80 = Ouro
-Se vitórias for entre 81 e 90 = Diamante
-Se vitórias for entre 91 e 100= Lendário
-Se vitórias for maior ou igual a 101 = Imortal
-*/
-
-const vitorias = 12
-const derrotas = 9
-
+patente(11, 4)
+function patente(v, d){
+    const vitorias = v
+    const derrotas = d
+    let nivel = ""
+    let rank = (vitorias - derrotas)
+    if(rank <= 10){
+        nivel = "ferro"
+    }
+    if(rank >= 11){
+        nivel = "bronze"
+    }
+    console.log(`O Herói tem de saldo de ${vitorias} vitórias e está no nivel ${nivel}`)
+}
